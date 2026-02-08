161b47955b886b694c1c907b8c4e9163bf5d2ecb
Geoordonez <geovanni.ordonez@galileo.edu>
2026-02-07 23:16:14 -0600

    implemetacio de songcard

--- app/src/main/java/com/example/lab3_fundamentos_web/ui/components/SongCard.kt
+++ app/src/main/java/com/example/lab3_fundamentos_web/ui/components/SongCard.kt
@@ -1,2 +1,34 @@
 package com.example.lab3_fundamentos_web.ui.components
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import com.example.lab3_fundamentos_web.Data.Model.Song
+
+@Composable
+fun SongCard(
+    song: Song,
+    onFavoriteClick: (String) -> Unit
+) {
+    Card {
+        ListItem(
+            headlineContent = { Text(song.title) },
+            supportingContent = { Text(song.artist) },
+            trailingContent = {
+                IconButton(onClick = { onFavoriteClick(song.id) }) {
+                    Icon(
+                        imageVector = if (song.isFavorite)
+                            Icons.Filled.Favorite
+                        else
+                            Icons.Outlined.FavoriteBorder,
+                        contentDescription = null
+                    )
+                }
+            }
+        )
+    }
+}
+
+
